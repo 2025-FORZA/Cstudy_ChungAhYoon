28aabcdb1ec5eb1c114b823ba46ee5445dc15af5
ahyoon7 <cay77@sookmyung.ac.kr>
2025-04-05 12:58:49 +0900

    1_Lab02

---
+++ 1_Lab02_1.c
@@ -0,0 +1,17 @@
+// c
+#if 01
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    long long A, B, C;
+
+    scanf("%lld %lld %lld", &A, &B, &C);
+
+    printf("%lld", A + B + C);
+
+
+    return 0;
+}
+#endif
